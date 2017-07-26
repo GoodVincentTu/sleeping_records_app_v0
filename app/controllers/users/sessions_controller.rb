e75b4bb94fa428f0cc39a3a782d62541679b432a
metatrontu@gmail.com <metatrontu@gmail.com>
2017-07-27 01:20:25 +0800

    User can register and login the website. The jwt will be given right after the user registered or signed in

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,33 @@
+class Users::SessionsController < Devise::SessionsController
+  # before_action :configure_sign_in_params, only: [:create]
+
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
+
+  # POST /resource/sign_in
+  def create
+    super
+    give_jwt_with_session
+  end
+
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
+
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
+
+  private
+
+  def give_jwt_with_session
+    return unless resource.present?
+    session[:jwt] = ::JsonWebToken.encode(sub: resource.id)
+  end
+end
--- doc/spec.md
+++ doc/spec.md
@@ -6,4 +6,4 @@
  - We need a RESTful API, to satisfy 3 requirements:
     1. [] The user can check-in and check-out, also check their check-in and check-out records (be able to sort by checking time.).
     2. [] The user can follow and unfollow another user’s checking records.
-    3. p[ The user can view all the following users’ sleeping records. (be able to sort by sleeping time length.) 
+    3. [] The user can view all the following users’ sleeping records. (be able to sort by sleeping time length.) 
